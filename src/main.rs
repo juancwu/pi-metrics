dd6eb85408a9ac0709084ffaae9b828108e01a9c
jc <46619361+juancwu@users.noreply.github.com>
2024-02-17 13:49:57 -0500

    add non functioning rust code rust async is confusing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-mod temperature;
+// mod temperature;
+
+use std::env;
 
 use dotenv::dotenv;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
-    let cpu_temp = temperature::get_cpu_temp().expect("Failed to get CPU temperature");
+    // these two have to be defined! (for now at least)
+    let url_string = env::var("LIBSQL_CLIENT_URL").unwrap();
+    let db_token = env::var("LIBSQL_CLIENT_TOKEN").unwrap();
+    let config = libsql_client::Config {
+        url: url::Url::parse(&url_string).unwrap(),
+        auth_token: db_token,
+    };
 
-    println!("CPU Temperature: {:.1}C", cpu_temp);
+    db.execute("CREATE TABLE IF NOT EXISTS metrics (id INTEGER PRIMARY KEY AUTOINCREMENT, temperature FLOAT);")
+        .await.unwrap();
+
+    // let cpu_temp = temperature::get_cpu_temp().expect("Failed to get CPU temperature");
+    //
+    println!("CPU Temperature: C");
 }
