938206eed457d70ca8ba38c24971a03a01e34433
jc <46619361+juancwu@users.noreply.github.com>
2024-02-17 00:00:29 -0500

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod temperature;
 
+use dotenv::dotenv;
+
 fn main() {
+    dotenv().ok();
+
     let cpu_temp = temperature::get_cpu_temp().expect("Failed to get CPU temperature");
 
     println!("CPU Temperature: {:.1}C", cpu_temp);
